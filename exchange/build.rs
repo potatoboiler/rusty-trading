00081d4e16b7b30107797599dfa732d86a641ea2
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-02-08 15:51:17 -0800

    Edit protobufs and build.rs

--- exchange/build.rs
+++ exchange/build.rs
@@ -8,5 +8,5 @@ fn main() {
     tonic_build::configure()
         .out_dir("src/proto")
         .compile(&["proto/exchange.proto"], &["proto/"])
-        .unwrap_or_else(|e| panic!("Failed to compile protos: {:?}", e));
+        .unwrap_or_else(|e| panic!("Failed to compile protos: {e:?}"));
 }
